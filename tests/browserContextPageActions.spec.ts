e12f498ef5f363b727c1ac0905b110d37409a5ad
Maram Hemanth Reddy <hmaram21@yahoo.com>
2024-11-02 13:45:47 +0530

    errror

--- tests/browserContextPageActions.spec.ts
+++ tests/browserContextPageActions.spec.ts
@@ -6,4 +6,10 @@ test(`invoke browser and create context page`, async({browser})=>{
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
     console.log(await page.title())
    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
+  await  page.locator('#username').fill('rahulshettyacademy')
+  await page.locator('#password').fill('learning1')
+  await page.locator('#signInBtn').click()
+ console.log( await page.locator('[style*="none"]').textContent())
+ expect (page.locator('[style*="none"]')).toContainText('Incorrects')
 })
+
